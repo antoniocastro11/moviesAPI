65c5e0ac5ae48b626edc62a17247c1605ac0fb74
castro <antoniog_castro@hotmail.com>
2026-06-22 17:40:41 -0400

    feat: inicio do projeto

---
+++ moviesApi/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using moviesAPi.Models;
+namespace moviesAPi.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+    }
+}
