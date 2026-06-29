af7f6cef46f8ceaa4c9d488b8456d3d6c9b4f991
castro <antoniog_castro@hotmail.com>
2026-06-29 07:18:48 -0400

    feat: utilização de dbcontext

---
+++ moviesApi/Data/MovieContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using moviesApi.Models;
+namespace moviesApi.Data;
+
+
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> opts) : base(opts)
+    {
+        //um context para cada banco de dados
+    }
+    public DbSet<Movie> Movies => Set<Movie>();// um set para cada entidade
+}
