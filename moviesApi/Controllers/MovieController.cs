dd960d38e69e5d815ae00931659b73be010b5cdd
castro <antoniog_castro@hotmail.com>
2026-06-29 07:20:40 -0400

    fix: corrige controle para adequar-se ao bd

--- moviesApi/Controllers/MovieController.cs
+++ moviesApi/Controllers/MovieController.cs
@@ -1,17 +1,37 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using moviesAPi.Models;
-namespace moviesAPi.Controllers; 
+using moviesApi.Data;
+using moviesApi.Data.Dtos;
+using moviesApi.Models;
+namespace moviesApi.Controllers; 
 
 [ApiController]
 [Route("[controller]")]
-
-public class MovieController : ControllerBase
+public class MovieController(MovieContext context, IMapper mapper) : ControllerBase
 {
-    private static List<Movie> movies = new List<Movie>();
 
     [HttpPost]
-    public void AddMovie([FromBody] Movie movie)
+    public IActionResult AddMovie([FromBody] CreateMovieDto movieDto)
     {
-        movies.Add(movie);
+        Movie movie = mapper.Map<Movie>(movieDto);
+        context.Movies.Add(movie);
+        context.SaveChanges();
+        return CreatedAtAction(nameof(GetMovieById), 
+        new { id = movie.Id}, movie);
+    }
+
+    [HttpGet]
+    public IEnumerable<Movie> GetMovies([FromQuery]int skip = 0, [FromQuery]int take = 10)
+    {
+        return context.Movies.Skip(skip).Take(take);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetMovieById(int id)
+    {
+        var movie = context.Movies.Find(id);
+        if (movie is null)
+            return NotFound();
+        return Ok(movie);
     }
 }
