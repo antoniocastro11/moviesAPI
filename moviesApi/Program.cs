94f0b1d8f977e59ffe95caea3f0cf89b6a7e0cca
castro <antoniog_castro@hotmail.com>
2026-09-17 12:01:28 -0400

    feat: add scalar

--- moviesApi/Program.cs
+++ moviesApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using moviesApi.Data;
 using moviesApi.Profiles;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connString  = builder.Configuration.GetConnectionString("DatabaseConnection");
@@ -25,6 +26,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
